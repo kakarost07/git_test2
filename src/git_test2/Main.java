78697eac1a347caaa46336414580089b28ddba15
Kakarost07 <kill0077777@gmail.com>
2025-01-21 11:29:49 +0900

    Main.java에 Hello h 오류 수정함

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -1,13 +1,13 @@
 package git_test2;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 
-		//Hrllo라는 객체 생성함.
-		Hello h new Hello();
+		//Hello라는 객체 생성함.
+		Hello h = new Hello();
 		//Hello라는 객체 메모리 내부에  say이름의 메소드의 호출!
 		h.say();
-
 	}
 }
+
